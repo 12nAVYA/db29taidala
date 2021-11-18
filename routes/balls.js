243a182c0968d3c3ae7f29c6dc61f67e1b40a3fe
12nAVYA <89415684+12nAVYA@users.noreply.github.com>
2021-11-17 21:57:52 -0600

    ballsdetail is added

--- routes/balls.js
+++ routes/balls.js
@@ -8,5 +8,6 @@ router.get('/', balls_controlers.balls_view_all_Page );
 router.get('/detail', balls_controlers.balls_view_one_Page);
 /* GET create balls page */
 router.get('/create', balls_controlers.balls_create_Page);
-
+/* GET create update page */
+router.get('/update', balls_controlers.balls_update_Page);
 module.exports = router; 
