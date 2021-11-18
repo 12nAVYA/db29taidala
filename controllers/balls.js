89fe999725f8a76fd5d1648d66d0e4eb0396fd12
12nAVYA <89415684+12nAVYA@users.noreply.github.com>
2021-11-17 21:30:43 -0600

    new files are added in controllers

--- controllers/balls.js
+++ controllers/balls.js
@@ -105,3 +105,16 @@ exports.balls_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+   // Handle building the view for creating a balls.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.balls_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('ballscreate', { title: 'balls Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/balls.js
+++ routes/balls.js
@@ -6,4 +6,7 @@ var router = express.Router();
 router.get('/', balls_controlers.balls_view_all_Page );
 /* GET detail balls page */
 router.get('/detail', balls_controlers.balls_view_one_Page);
+/* GET create balls page */
+router.get('/create', balls_controlers.balls_create_Page);
+
 module.exports = router; 
