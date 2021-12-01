1cad90f1e633c537619efda4faaaedc2b9b68ce9
12nAVYA <89415684+12nAVYA@users.noreply.github.com>
2021-11-30 23:42:02 -0600

    protected end points

--- routes/balls.js
+++ routes/balls.js
@@ -1,15 +1,23 @@
 var express = require('express');
 const balls_controlers= require('../controllers/balls');  
 var router = express.Router();
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 /* GET home page. */
 router.get('/', balls_controlers.balls_view_all_Page );
 /* GET detail balls page */
 router.get('/detail', balls_controlers.balls_view_one_Page);
 /* GET create balls page */
-router.get('/create', balls_controlers.balls_create_Page);
+router.get('/create', secured,balls_controlers.balls_create_Page);
 /* GET create update page */
-router.get('/update', balls_controlers.balls_update_Page);
+router.get('/update', secured,balls_controlers.balls_update_Page);
 /* GET create costume page */
-router.get('/delete', balls_controlers.balls_delete_Page);
+router.get('/delete', secured,balls_controlers.balls_delete_Page);
 module.exports = router; 
+
