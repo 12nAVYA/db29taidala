ff0486276c5405c904ae9c47ba57166c8b967f4c
12nAVYA <89415684+12nAVYA@users.noreply.github.com>
2021-11-30 23:22:01 -0600

    update and detail are changed

--- models/balls.js
+++ models/balls.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose")
 const ballsSchema = mongoose.Schema({
 name: String,
 color: String,
-count: Number
+count: {type:Number,min:4,max:100}
 })
 module.exports = mongoose.model("balls",ballsSchema)
